c622a58b3305f0e8581268a106d4f3eec2c80092
9caihezi <9caihezi@gmail.com>
2019-12-29 13:27:48 +0000

    Modify test server.

--- src/asio/test_server.cpp
+++ src/asio/test_server.cpp
@@ -1,47 +1,148 @@
 #include <ctime>
 #include <iostream>
+#include <memory>
 #include <string>
+#include <boost/bind.hpp>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
+#include <boost/enable_shared_from_this.hpp>
 
+using boost::asio::io_context;
 using boost::asio::ip::tcp;
+using std::cout;
+using std::enable_shared_from_this;
+using std::endl;
+using std::make_shared;
+using std::shared_ptr;
+using std::string;
 
-std::string make_daytime_string() {
+//const string GREETING("Hello\n");
+
+shared_ptr<string> MakeDaytimeString() {
   std::time_t now = std::time(0);
-  return std::ctime(&now);
+  return make_shared<string>(std::ctime(&now));
 }
+
+class Connection : public enable_shared_from_this<Connection> {
+public:
+  ~Connection() {
+    cout << "Connection distructed." << endl;
+  }
+
+  static shared_ptr<Connection> Create(io_context& context) {
+    //return make_shared<Connection>(context);
+    return shared_ptr<Connection>(new Connection(context));
+  }
+  tcp::socket& Socket() {
+    return socket_;
+  }
+
+  void Start() {
+    Write(make_shared<string>("Hello!\n"));
+    Read();
+  }
+private:
+  Connection(boost::asio::io_context& context)
+    : socket_(context) {
+    cout << "Connection created." << endl;
+  }
+
+  void Write(const shared_ptr<string>& message) {
+    boost::asio::async_write(
+        socket_,
+        boost::asio::buffer(*message),
+        boost::bind(
+          &Connection::HandleWrite,
+          shared_from_this(),
+          message,
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+  }
+  void Read() {
+    cout << "Waiting for data." << endl;
+    socket_.async_receive(
+        boost::asio::buffer(buf_),
+        boost::bind(
+          &Connection::HandleRead,
+          shared_from_this(),
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+  }
+  void HandleWrite(
+      shared_ptr<string> message,
+      const boost::system::error_code& error,
+      size_t bytes_transferred) {
+    if (error) {
+      // TODO: try cout << error << endl;
+      //throw boost::system::system_error(error);
+      cout << error << endl;
+    }
+    cout << "Message sent (" << bytes_transferred << "):" << *message << endl;
+  }
+
+  void HandleRead(
+      const boost::system::error_code& error,
+      size_t bytes_transferred) {
+    if (error) {
+      // TODO: try cout << error << endl;
+      cout << error << endl;
+      return;
+    }
+    std::string received(buf_.data(), bytes_transferred);
+
+    cout << "Message received : " << received << endl;
+
+    auto message = MakeDaytimeString();
+    message->pop_back();
+    *message += "--->" + received + "\n";
+    std::cout << "Sending message: " << *message << std::endl;
+
+    Write(message);
+    Read();
+  }
+
+  boost::array<char, 128> buf_;
+  tcp::socket socket_;
+};
+
+class Server {
+public:
+  Server(io_context& context)
+    : context_(context),
+      acceptor_(context, tcp::endpoint(tcp::v4(), 10024)) {
+    start_accept();
+  }
+
+  void start_accept() {
+    auto connection = Connection::Create(context_);
+    acceptor_.async_accept(
+        connection->Socket(),
+        boost::bind(&Server::handle_accept,
+                    this,
+                    connection,
+                    boost::asio::placeholders::error));
+  }
+
+  void handle_accept(
+    shared_ptr<Connection> connection,
+    const boost::system::error_code& ec) {
+    cout << "Connected." << endl;
+    if (!ec) {
+      connection->Start();
+    }
+    start_accept();
+  }
+private:
+  io_context& context_;
+  tcp::acceptor acceptor_;
+};
+
 
 int main() {
   try {
-    boost::asio::io_context io_context;
-
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 10024));
-
-    boost::system::error_code error;
-
-    tcp::socket socket(io_context);
-    acceptor.accept(socket);
-    std::string greeting = "Hello!\n";
-    boost::asio::write(socket, boost::asio::buffer(greeting), error);
-
-    while (true) {
-      boost::array<char, 128> buf;
-
-      size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-      if (error)
-        throw boost::system::system_error(error); // Some other error.
-
-      std::string receivedMessage(buf.data(), len);
-      std::string message = make_daytime_string();
-      message.pop_back();
-      message += "--->" + receivedMessage + "\n";
-      std::cout << "Send message: " << message << std::endl;
-
-      boost::asio::write(socket, boost::asio::buffer(message), error);
-      if (error)
-        throw boost::system::system_error(error); // Some other error.
-    }
+    boost::asio::io_context context;
+    Server server(context);
+    context.run();
   }
   catch (std::exception& e)
   {
